54dc8391530ebac88d9b508233a418eb849a16b8
shreyas bole <shreyasbole4@gmail.com>
2024-02-12 22:53:47 +0530

    Refactor order routes and controllers

--- src/routes/order.ts
+++ src/routes/order.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import { adminOnly } from "../middlewares/auth.js";
 import {
-  //   allOrders,
-  //   deleteOrder,
-  //   getSingleOrder,
-  //   myOrders,
+  allOrders,
+  getSingleOrder,
+  deleteOrder,
+  myOrders,
   newOrder,
-  //   processOrder,
+  processOrder,
 } from "../controllers/order.js";
 
 const app = express.Router();
@@ -15,14 +15,15 @@ const app = express.Router();
 app.post("/new", newOrder);
 
 // route - /api/v1/order/my
-// app.get("/my", myOrders);
+app.get("/my", myOrders);
 
 // route - /api/v1/order/my
-// app.get("/all", adminOnly, allOrders);
+app.get("/all", adminOnly, allOrders);
 
-app.route("/:id");
-//   .get(getSingleOrder)
-//   .put(adminOnly, processOrder)
-//   .delete(adminOnly, deleteOrder);
+app
+  .route("/:id")
+  .get(getSingleOrder)
+  .put(adminOnly, processOrder)
+  .delete(adminOnly, deleteOrder);
 
 export default app;
