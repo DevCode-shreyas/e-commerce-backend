42423f9f37e6a638fc4cf135bbc1e6ae3a59bb65
shreyas bole <shreyasbole4@gmail.com>
2024-02-27 18:57:36 +0530

    Add Stripe payment integration

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import { errorMiddleware } from "./middlewares/error.js";
 import NodeCache from "node-cache";
 import { config } from "dotenv";
 import morgan from "morgan";
+import Stripe from "stripe";
 
 // import routes
 import userRoute from "./routes/user.js";
@@ -19,10 +20,12 @@ config({
 });
 
 const port = process.env.PORT || 4000;
-
 const mongoURI = process.env.MONGO_URI || "";
+const stripeKey = process.env.STRIPE_KEY || "";
 
 connectDB(mongoURI);
+
+export const stripe = new Stripe(stripeKey);
 
 export const myCache = new NodeCache();
 
